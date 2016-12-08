5f10e80b6825350c8197b5c57a5c00a41f89923a
Gyujin Cho <gyujincho@ridi.com>
2016-12-08 20:19:09 +0900

    Make main class

--- svgbg.js
+++ svgbg.js
@@ -6,72 +6,119 @@ const map = require('async/map');
 const SVGO = require('./plugins/customSvgo');
 const TemplateBuilder = require('./plugins/templateBuilder');
 
-//// svg min
-const svgoConfig = {
-  plugins: [
-    { removeTitle: true },
-    { removeDimensions: true },
-    { removeStyleElement: true },
-    { removeAttrs: { attrs: 'path:(fill|id|clip-path|fill-rule|clip-rule)' }}
-  ]
+const config = {
+  src: path.resolve(__dirname, './test/src'),
+  minify: {
+    dest: path.resolve(__dirname, './test/dist'),
+    options: {
+      plugins: [
+        { removeTitle: true },
+        { removeDimensions: true },
+        { removeStyleElement: true },
+        { removeAttrs: { attrs: 'path:(fill|id|clip-path|fill-rule|clip-rule)' }}
+      ],
+    }
+  },
+  background: {
+    name: 'sample',
+    type: 'less',
+    dest: [ path.resolve(__dirname, './test/dist') ],
+    template: path.resolve(__dirname, './test/templates/sample.less.hbs'),
+  },
+  demo: {
+    name: 'sample',
+    type: 'html',
+    dest: [ path.resolve(__dirname, './test/dist') ],
+    template: path.resolve(__dirname, './test/templates/sample.html.hbs'),
+  },
 };
 
-const svgo = new SVGO(svgoConfig);
-const srcPath = path.resolve(__dirname, './test/src');
-const destPath = path.resolve(__dirname, './test/dist');
-const svgList = fs.readdirSync(srcPath).filter(junk.not);
-console.log(svgList);
-const minifiedSvgList = [];
+class SvgGenerator {
+  constructor (config) {
+    this.config = config;
+    this.svgList = fs.readdirSync(this.config.src).filter(junk.not);
+    this.minifiedSvgList = [];
+    this.minify = this.minify.bind(this);
+    this.buildTemplate = this.buildTemplate.bind(this);
+  }
 
-each(svgList, (filename) => {
-  const srcSvg = fs.readFileSync(path.join(srcPath, filename));
+  minify () {
+    const { src } = this.config;
+    const { dest, options } = this.config.minify;
+    const svgo = new SVGO(options);
 
-  svgo.optimize(srcSvg, (minifiedSvgJs, minifiedSvgStr) => {
-    fs.writeFileSync(path.resolve(destPath, filename), minifiedSvgStr);
-    const svgJs = minifiedSvgJs.content[0];
+    each(this.svgList, (filename) => {
+      const srcSvg = fs.readFileSync(path.join(src, filename));
 
-    function sortPaths (list) {
-      return list.map((element) => {
-        if (element.elem === 'g') {
-          return sortPaths(element.content);
-        } else if (element.elem === 'path') {
-          return element.attrs.d.value;
-        } else {
-          console.log(element);
+      svgo.optimize(srcSvg, (minifiedSvgJs, minifiedSvgStr) => {
+        fs.writeFileSync(path.resolve(dest, filename), minifiedSvgStr);
+        const svgJs = minifiedSvgJs.content[0];
+
+        function sortPaths (list) {
+          return list.map((element) => {
+            if (element.elem === 'g') {
+              return sortPaths(element.content);
+            } else if (element.elem === 'path') {
+              return element.attrs.d.value;
+            } else {
+              console.log(element);
+            }
+          });
         }
+
+        const svgForTemplate = {
+          name: filename.substr(0, filename.lastIndexOf('.')),
+          viewBox: {
+            width: svgJs.attrs.viewBox.value.split(' ')[2],
+            height: svgJs.attrs.viewBox.value.split(' ')[3],
+          },
+          pathList: sortPaths(svgJs.content).concat().filter((element)=>{
+            if (element !== undefined) {
+              return element;
+            }
+          }),
+          fullSvgStr: minifiedSvgStr,
+        };
+
+        this.minifiedSvgList.push(svgForTemplate);
       });
-    }
 
-    const svgForTemplate = {
-      name: filename.substr(0, filename.lastIndexOf('.')),
-      viewBox: {
-        width: svgJs.attrs.viewBox.value.split(' ')[2],
-        height: svgJs.attrs.viewBox.value.split(' ')[3],
-      },
-      pathList: sortPaths(svgJs.content).concat().filter((element)=>{
-        if (element !== undefined) {
-          return element;
-        }
-      }),
-    };
+    });
+  }
 
-    minifiedSvgList.push(svgForTemplate);
-  });
-});
+  logMinSvgList () {
+    each(this.minifiedSvgList, (svg) => {
+      console.log(svg);
+    });
+  }
 
-each(minifiedSvgList, (svg) => {
-  console.log(svg);
-});
+  buildTemplate () {
+    const templateBuilder = new TemplateBuilder(this.minifiedSvgList);
+    // templateBuilder.build([
+    //   {
+    //     srcPath: path.resolve(__dirname, './test/templates/sample.less.hbs'),
+    //     destPathList: [ path.resolve(__dirname, './test/dist') ]
+    //   },
+    //   {
+    //     srcPath: path.resolve(__dirname, './test/templates/sample.html.hbs'),
+    //     destPathList: [ path.resolve(__dirname, './test/dist') ]
+    //   },
+    // ]);
+    templateBuilder.build([
+      this.config.background,
+      this.config.inline,
+      this.config.demo,
+    ]);
+  }
 
-//// template build
-const templateBuilder = new TemplateBuilder(minifiedSvgList);
-templateBuilder.build([
-  {
-    srcPath: path.resolve(__dirname, './test/templates/sample.less.hbs'),
-    destPathList: [ path.resolve(__dirname, './test/dist') ]
-  },
-  {
-    srcPath: path.resolve(__dirname, './test/templates/sample.html.hbs'),
-    destPathList: [ path.resolve(__dirname, './test/dist') ]
-  },
-]);
+  build () {
+    this.minify();
+    this.logMinSvgList();
+    this.build();
+  }
+}
+
+const svgGen = SvgGenerator(config);
+svgGen.build();
+
+module.exports = SvgGenerator;
